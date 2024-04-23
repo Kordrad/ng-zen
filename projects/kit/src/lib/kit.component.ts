ebb10285cb245f965bab379ad458c3416c3ec270
Kordrad <kord.stp@gmail.com>
2024-04-23 19:03:50 +0200

    feat(kit): add new library

---
+++ projects/kit/src/lib/kit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zen-kit',
+  standalone: true,
+  template: ` <p>kit works!</p> `,
+})
+export class KitComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { KitComponent } from '@zen-hubs/ng-zen/lib/kit.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, KitComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
