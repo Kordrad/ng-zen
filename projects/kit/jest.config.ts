06b7e3acf4d947ee8ee79bde772d987d813bbf62
Kordrad <kord.stp@gmail.com>
2024-05-08 15:06:16 +0200

    feat(badge): add new component (#10)

---
+++ projects/kit/jest.config.ts
@@ -0,0 +1,6 @@
+import type { Config } from 'jest';
+import Root from '../../jest.config';
+
+export default {
+  ...Root,
+} satisfies Config;
---
+++ projects/kit/src/components/badge/badge.component.ts
@@ -0,0 +1,21 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'zen-badge,[zenBadge]',
+  standalone: true,
+  template: `
+    <ng-content select="[topRight]" />
+    <ng-content select="[topLeft]" />
+    <ng-content select="[bottomRight]" />
+    <ng-content select="[bottomLeft]" />
+    <ng-content />
+  `,
+  styleUrl: './badge.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ZenBadgeComponent {}
