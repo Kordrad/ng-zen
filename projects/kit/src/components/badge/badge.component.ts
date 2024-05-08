6ad802cfe50958904a02d4cae919e31475b3ed18
Kordrad <kord.stp@gmail.com>
2024-05-08 09:46:16 +0200

    feat(badge): finish badge component

--- projects/kit/src/components/badge/badge.component.ts
+++ projects/kit/src/components/badge/badge.component.ts
@@ -1,35 +1,21 @@
-import { NgTemplateOutlet } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  computed,
-  input,
-  InputSignal,
-  TemplateRef,
   ViewEncapsulation,
 } from '@angular/core';
 
-import { TypeofPipe } from './typeof.pipe';
-
 @Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
-  selector: '[zenBadge]',
+  selector: 'zen-badge',
   standalone: true,
-  imports: [TypeofPipe, NgTemplateOutlet],
-  templateUrl: './badge.component.html',
+  template: `
+    <ng-content select="[topRight]" />
+    <ng-content select="[topLeft]" />
+    <ng-content select="[bottomRight]" />
+    <ng-content select="[bottomLeft]" />
+    <ng-content />
+  `,
+  styleUrls: ['./badge.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ZenBadgeComponent {
-  readonly badge: InputSignal<
-    number | string | boolean | TemplateRef<unknown>
-  > = input.required({
-    alias: 'zenBadge',
-  });
-  readonly badgeTemplate = computed<TemplateRef<unknown> | null>(() => {
-    if (!['string', 'number', 'boolean'].includes(typeof this.badge())) {
-      return this.badge() as TemplateRef<unknown>;
-    }
-    return null;
-  });
-}
+export class ZenBadgeComponent {}
