a10ce8cea08ce9777594df6b3bf1137278cb7f30
Kordrad <kord.stp@gmail.com>
2024-05-14 20:45:32 +0200

    feat(storybook): configure project
    additionally:
    - add badge stories

--- projects/kit/src/components/badge/badge.component.ts
+++ projects/kit/src/components/badge/badge.component.ts
@@ -4,6 +4,24 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 
+/**
+ * ZenBadgeComponent is a versatile Angular component used to display badges or indicators
+ * in various positions within its parent container. It allows for flexible placement
+ * of badge content such as icons, text, or other elements in the top-left, top,
+ * top-right, left, right, bottom-left, bottom, and bottom-right positions.
+ *
+ * This component offers standalone usage and is highly customizable through its
+ * template and styling. It leverages Angular's change detection strategy 'OnPush'
+ * for optimal performance.
+ *
+ * Additionally, it provides support for CSS variables to customize badge positioning:
+ *   - --zen-badge-offset: Controls the overall offset of the badge. Default is 'unset'.
+ *   - --zen-badge-offset-x: Controls the horizontal offset of the badge. Default is 'unset'.
+ *   - --zen-badge-offset-y: Controls the vertical offset of the badge. Default is 'unset'.
+ *
+ *
+ * @summary ZenBadgeComponent: Angular component for flexible badge placement, standalone usage, and CSS variable support for easy customization.
+ */
 @Component({
   selector: 'zen-badge,[zenBadge]',
   standalone: true,
