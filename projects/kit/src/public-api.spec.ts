de43d0b9d8ef7fc6f1c99e55fecb7503bb2098cd
Kordrad <kord.stp@gmail.com>
2024-06-18 07:28:51 +0200

    test(kit): remove tests for kit library

--- projects/kit/src/public-api.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as fileElements from './public-api';
-
-describe('public-api', () => {
-  it('should export library components', () => {
-    expect(Object.keys(fileElements)).toBeTruthy();
-  });
-});
