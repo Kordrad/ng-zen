5a5482650ee12b7fe79c0b166a4e8d3033667796
Konrad <kord.stp@gmail.com>
2024-06-20 11:34:59 +0200

    feat(tag): add component (#35)

---
+++ projects/cli/schematics/components/files/tag/tag.component.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+/**
+ * The `ZenTagComponent` is a reusable UI component designed to display a tag or label.
+ * It allows for custom content to be projected inside the tag.
+ *
+ * ```html
+ * <zen-tag>...</zen-tag>
+ * ```
+ *
+ * @example <zen-tag>Simple tag</zen-tag>
+ * @component
+ * @selector `zen-tag`
+ *
+ * @license BSD-2-Clause
+ * @author Konrad Stępień <kord.stp@gmail.com>
+ * @link https://github.com/Kordrad/ng-zen
+ */
+@Component({
+  selector: 'zen-tag',
+  standalone: true,
+  template: `<ng-content />`,
+  styleUrl: './tag.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ZenTagComponent {}
