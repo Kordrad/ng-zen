a6028d5d3bc00fd5f13bf51192225e2eda68e0fc
Konrad <kord.stp@gmail.com>
2024-06-25 11:03:28 +0200

    feat(switch): add component (#56)

---
+++ projects/cli/stories/components/switch.stories.ts
@@ -0,0 +1,43 @@
+import { FormsModule } from '@angular/forms';
+import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
+
+import { ZenSwitchComponent } from '../../schematics/components/files/switch';
+
+export default {
+  title: 'Components/Switch',
+  component: ZenSwitchComponent,
+  tags: ['autodocs'],
+  decorators: [
+    moduleMetadata({
+      imports: [FormsModule],
+    }),
+  ],
+  render: args => ({ props: { ...args } }),
+} satisfies Meta<ZenSwitchComponent>;
+
+type Story = StoryObj<ZenSwitchComponent>;
+
+export const Default: Story = {
+  render: () => ({
+    template: `
+        <zen-switch />
+    `,
+  }),
+};
+
+export const Disabled: Story = {
+  render: () => ({
+    template: `<zen-switch [disabled]="true" />`,
+  }),
+};
+
+export const Checked: Story = {
+  render: () => ({
+    template: `
+      <zen-switch [ngModel]="true" />
+      <br/>
+      <zen-switch [checked]="true" />
+    `,
+  }),
+};
