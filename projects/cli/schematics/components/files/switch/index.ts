23da8bf382fbc62f40383d1a37d1112df777b596
Konrad <kord.stp@gmail.com>
2024-06-25 11:03:28 +0200

    feat(switch): add component (#56)

---
+++ projects/cli/schematics/components/files/switch/index.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { ZenSwitchComponent } from './zen-switch.component';
+
+@NgModule({
+  imports: [ZenSwitchComponent],
+  exports: [ZenSwitchComponent],
+})
+export class ZenSwitchModule {}
+export * from './zen-switch.component';
