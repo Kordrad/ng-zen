71a52d00afffa9567033447c40538809bcba6262
Kordrad <kord.stp@gmail.com>
2024-06-27 14:37:36 +0200

    feat(checkbox): add component

---
+++ projects/cli/schematics/components/files/checkbox/checkbox.component.ts
@@ -0,0 +1,148 @@
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  ElementRef,
+  forwardRef,
+  inject,
+  model,
+  Renderer2,
+  viewChild,
+} from '@angular/core';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+import { map } from 'rxjs';
+
+type CheckedState = boolean | 'mixed';
+type OnChangeFn = (value: CheckedState) => void;
+type OnTouchedFn = () => void;
+
+/**
+ * ZenCheckboxComponent is a custom checkbox component implementing ControlValueAccessor.
+ * It supports a tri-state checkbox (checked, unchecked, and indeterminate).
+ *
+ * @example
+ * <zen-checkbox />
+ *
+ * @export
+ * @class ZenCheckboxComponent
+ * @implements {ControlValueAccessor}
+ * @implements {AfterViewInit}
+ *
+ * @license BSD-2-Clause
+ * @author Konrad Stępień
+ * @link https://github.com/Kordrad/ng-zen
+ */
+@Component({
+  selector: 'zen-checkbox',
+  standalone: true,
+  templateUrl: './checkbox.component.html',
+  styleUrl: './checkbox.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ZenCheckboxComponent),
+      multi: true,
+    },
+  ],
+  imports: [FormsModule],
+})
+export class ZenCheckboxComponent
+  implements ControlValueAccessor, AfterViewInit
+{
+  /** Model for the checked state of the checkbox. */
+  readonly checked = model<CheckedState>(false);
+
+  /** Model for the disabled state of the checkbox. */
+  readonly disabled = model<boolean>(false);
+
+  /** @ignore */
+  private readonly checked$ = toObservable(this.checked);
+
+  /** @ignore */
+  private readonly destroyRef = inject(DestroyRef);
+  /** @ignore */
+  private readonly renderer2 = inject(Renderer2);
+
+  /** @ignore */
+  private readonly inputElement =
+    viewChild<ElementRef<HTMLInputElement>>('inputElement');
+
+  /**
+   * Lifecycle hook called after Angular has fully initialized a component's view.
+   * Initializes the indeterminate state of the checkbox.
+   *
+   * @ignore
+   */
+  ngAfterViewInit(): void {
+    this.initIndeterminate();
+  }
+
+  private onChange: OnChangeFn = () => {};
+  private onTouched: OnTouchedFn = () => {};
+
+  /**
+   * Writes a new value to the component.
+   */
+  writeValue(value: boolean): void {
+    this.checked.set(value);
+  }
+
+  /**
+   * Registers a function to be called when the value changes.
+   */
+  registerOnChange(fn: OnChangeFn): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Registers a function to be called when the component is touched.
+   */
+  registerOnTouched(fn: OnTouchedFn): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Sets the disabled state of the component.
+   */
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
+  }
+
+  /**
+   * Toggles the checkbox value and notifies the change.
+   * If the component is disabled, no action is performed.
+   */
+  onToggle(): void {
+    if (this.disabled()) return;
+
+    this.checked.update(value => !value);
+    this.onChange(this.checked());
+    this.onTouched();
+  }
+
+  /**
+   * Initializes the indeterminate state of the checkbox based on the checked state.
+   * If the checked state is 'mixed', the checkbox will be set to indeterminate.
+   */
+  private initIndeterminate(): void {
+    this.checked$
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        map(value => value === 'mixed')
+      )
+      .subscribe(value => {
+        this.renderer2.setProperty(
+          this.inputElement()?.nativeElement,
+          'indeterminate',
+          value
+        );
+      });
+  }
+}
---
+++ projects/cli/stories/components/checkbox.stories.ts
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { ZenCheckboxComponent } from '../../schematics/components/files/checkbox/checkbox.component';
+
+export default {
+  title: 'Components/Checkbox',
+  component: ZenCheckboxComponent,
+  tags: ['autodocs'],
+  render: args => ({ props: { ...args } }),
+} satisfies Meta<ZenCheckboxComponent>;
+
+type Story = StoryObj<ZenCheckboxComponent>;
+
+export const Default: Story = {
+  render: () => ({
+    template: `
+        <zen-checkbox />
+    `,
+  }),
+};
+
+export const Mixed: Story = {
+  render: () => ({
+    template: `
+        <zen-checkbox checked="mixed"/>
+    `,
+  }),
+};
+
+export const Diabled: Story = {
+  render: () => ({
+    template: `
+        <zen-checkbox disabled="true"/>
+        <zen-checkbox [checked]="true" disabled="true"/>
+        <zen-checkbox checked="mixed" disabled="true"/>
+
+    `,
+  }),
+};
