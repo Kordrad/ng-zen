be372891aece25127c3ebbfa2da1b0b022ec8355
Konrad Stępień <kord.stp@gmail.com>
2025-02-17 18:14:04 +0100

    fix(components): remove unnecessary modules export

--- projects/cli/src/schematics/components/files/avatar/index.ts
+++ projects/cli/src/schematics/components/files/avatar/index.ts
@@ -1,10 +1,1 @@
-import { NgModule } from '@angular/core';
-
-import { ZenAvatarComponent } from './avatar.component';
-
-@NgModule({
-  imports: [ZenAvatarComponent],
-  exports: [ZenAvatarComponent],
-})
-export class ZenButtonModule {}
 export * from './avatar.component';
